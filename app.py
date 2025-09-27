95f38051da832b4277b6c593132d4136dfd749c9
abcd771321-crypto <abcd771321@gmail.com>
2025-09-28 00:20:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ Run options:
 """
 
 import os
-
 from crypto_analysis import create_app
 
 app = create_app()
 
-
 if __name__ == "__main__":
     debug_env = os.getenv("FLASK_DEBUG", "1")
     debug = True if str(debug_env).lower() in ("1", "true", "yes", "y") else False
-    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
-    port = int(os.getenv("FLASK_RUN_PORT", "5000"))
+
+    # Compatible with Railway
+    host = "0.0.0.0"
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(debug=debug, host=host, port=port)
